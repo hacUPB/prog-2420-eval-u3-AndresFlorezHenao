d89ed45286adf8a92a59f18891f336033cc14416
Andres <andres.florezhenao179@gmail.com>
2024-10-11 20:57:56 -0500

    nuevo commit

--- README.md
+++ README.md
@@ -2,6 +2,58 @@
 # Unidad 3
 ---
 ## Documentación del proyecto
-Nombre:  
-ID:  
+Nombre:  Andres Felipe Florez Henao
+ID:  000540643
 ---
+
+- **Título del Proyecto:** Colegio
+- **Descripción Detallada:**
+    
+    Este problema consiste en que uno pueda llevar la actividad académica de los estudiantes en un colegio y tambien registrar los nuevos que lleguen cada año; esto es importante debido a que siempre se necesita llevar un soporte del estatus de los estudiantes dentro de la institución educativa en caso de querer actualizar datos como notas o tambien la consulta de estas, ademas, cada año llegan nuevos estudiantes y la institucion debe estar en la . El programa se va a encontrar en el archivo "Datos.py" en la carpeta "src" y tambien se recomienda ingresar al archivo "user guide", ahi se encuentra toda la informacion necesaria para que el usuario ejecute de manera adecuada el programa
+    
+- **Alcance:**
+
+    el programa guardará el primer nombre, edad, ID institucional, periodo, notas y el grado de cada estudiante; el usuario podrá buscar tanto la información del estudiante por su ID o ver el estatus de todos los estudiantes en general, esto es necesario para los casos en que un estudiante desee revisar sus notas o sus datos en el sistema del colegio
+- **Estructuras de Datos Utilizadas:**
+
+    listas para llevar las notas del estudiante, diccionarios dentro de un diccionario que serviran para llevar los datos del estudiante.
+- **Diagrama de Flujo o Pseudocódigo:**
+
+        INICIO
+        Variable control = Verdadero
+        Mientras control sea Verdadero:
+            imprimir mensaje de bienvenida y menú de opciones
+            leer opción del usuario
+            limpiar pantalla usando "system('cls')"
+
+            Si opción es 1:
+                Leer id del estudiante
+                Llamar a función añadir_notas(id)       
+                
+                 // esta funcion sirve para agregar una nota de un estudiante por medio de un id
+
+            sino si opción es 2:
+                Leer nombre del nuevo estudiante
+                Leer id del nuevo estudiante
+                Leer edad del nuevo estudiante
+                Leer periodo del nuevo estudiante
+                Crear lista vacía notas
+                Leer grado del nuevo estudiante
+                Llamar a función registrar_estudiante(nombre, id, edad, periodo, notas, grado) 
+                // esta funcion sirve para agregar un estudiante al diccionario por medio de un id
+
+                Imprimir "Estudiante Registrado exitosamente"
+
+            Sino si opción es 3:
+                Imprimir diccionario estudiantes
+
+            Sino si opción es 4:
+                Leer id del estudiante que desea revisar
+                Llamar a función buscar_estudiante(id)
+                Imprimir valor retornado por buscar_estudiante
+
+            Sino si opción es 5:
+                Asignar control = Falso
+                Imprimir "gracias, vuelva pronto"
+                Terminar bucle
+        FIN
--- src/Datos.py
+++ src/Datos.py
@@ -1,8 +1,88 @@
+from os import system
+estudiantes={'pepe':{'nombre':'pepe', 'id':123, 'edad':12, 'periodo':1, 'Notas': [], 'grado' :3},
+            'ana':{'nombre':'ana', 'id':456, 'edad':12, 'periodo':1, 'Notas': [], 'grado':5},
+            'jeronimo':{'nombre':'jeronimo', 'id':789, 'edad':12, 'periodo':1, 'Notas': [], 'grado':3}
+}
+def añadir_notas(id:int):
+    cont=1
+    while cont==1:
+        estudiante=buscar_estudiante(id)
+        print("Bienvenido/a al directorio de", estudiante) 
+        nota=float(input("ingrese una nota de 0 a 5: "))
+        if nota<0 or nota>5:
+            print("nota no valida")
+            cont=cont
+        else:
+            for estudiante, di in estudiantes.items():
+                if di['id'] == id:
+                    y={di['Notas'].append(nota)}
+                    print("nota registrada correctmente")
+                    cont=cont-1
+                    return nota
+        
+#Funcion para buscar un estudiante
+def buscar_estudiante(id:int):
+    for estudiante,di in estudiantes.items():
+        if di['id'] == id:
+            print(
+    f"Los datos del estudiante son:\n"
+    f" Nombre: {di['nombre']},\n"
+    f" id: {di['id']},\n"
+    f" Edad: {di['edad']},\n"
+    f" periodo: {di['periodo']},\n"
+    f" Notas: {di['Notas']},\n"
+    f" Grado: {di['grado']}"
+)
+            return estudiante
+    print(f"Persona con id '{id}' no encontrada.")
 
+#Funcion para registrar un estudiante
+def registrar_estudiante(nombre, id, edad, periodo, notas, grado):
+    estudiantes[nombre] = {
+        'nombre': nombre,
+        'id':id,
+        'edad':edad,
+        'periodo': periodo,
+        'Notas': notas,
+        'grado': grado
+    }
+    return
+# Función para imprimir cada estudiante en renglones separados
+def imprimir_estudiantes(estudiantes):
+    for nombre, detalles in estudiantes.items():
+        print(f"Estudiante: {nombre}")
+        for atributo, valor in detalles.items():
+            print(f"  {atributo}: {valor}")
+        print()
+        
 def main():
-    #Tu código va aquí. Mantén la indentación
-    pass # borra esta línea cuando con inicies tu código
-
-
+    control=True #Variable de control para crear una lista en bucle
+    while control==True:
+        print(("bienvenido al colegio los angeles tunja, ¿Que opcion desea realizar?"
+               "\n 1. Ingresar nota \n 2. ingresar nuevo estudiante"
+               "\n 3. ver directorio de estudiantes \n 4. buscar estudiante \n 5. salir")) #entrega unas opciones para preguntar que desea el usuario
+        opcion=int(input("ingrese su opción: ")) #el usuario ingresa su opcion
+        system("cls")
+        if opcion==1:
+            id=int(input("ingrese el id del estudiante al que desea ingresar una nota: "))#aqui el usuario ingresa el ID del estudiante que desee revisar
+            añadir_notas(id)
+        elif opcion==2:
+            nombre=input("ingrese el nombre del nuevo estudiante: ")
+            id=int(input("ingrese el id del nuevo estudiante: ")) #aqui el usuario ingresa el ID del estudiante que desee revisar
+            edad=input("ingrese la edad del nuevo estudiante: ")
+            periodo=int(input("ingrese el periodo del nuevo estudiante:"))
+            notas=[]
+            grado=int(input("ingrese el grado del nuevo estudiante: "))
+            registrar_estudiante(nombre, id, edad, periodo, notas, grado)
+            print("Estudiante Registrado exitosamente")
+        elif opcion==3:
+            imprimir_estudiantes(estudiantes)
+        elif opcion==4:
+            id=int(input("ingrese el id del estudiante que desea revisar: "))
+            buscar_estudiante(id)
+        elif opcion==5:
+            control=False
+            print("sesion cerrada correctamente.\n vuelva pronto")
+            break
 if __name__ == "__main__":
     main()
